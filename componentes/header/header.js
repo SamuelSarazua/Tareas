3d0f45171bcc4e6620fc08b076daccb20bb89764
unknown <sesarazua@scl.edu.gt>
2025-02-04 13:44:47 -0600

    Se finaliza la primer versión del Header

--- componentes/header/header.js
+++ componentes/header/header.js
@@ -2,6 +2,15 @@ function header(){
     let header = document.createElement(`header`);
     header.className = "div-header";
 
+    let logo = document.createElement(`img`);
+    logo.src = "https://1000marcas.net/wp-content/uploads/2020/01/Lamborghini-Logo-tumb.png";
+    logo.alt = "Logo";
+    header.appendChild(logo);
+
+    let h1 = document.createElement(`h1`);
+    h1.textContent = "Todo-List";
+    header.appendChild(h1);
+
     return header;
 }
 
